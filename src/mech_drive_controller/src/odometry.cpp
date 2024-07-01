16399fe5bd8434d8dee6c2209bd8775eebde254e
muib <muib.akinyele@gmail.com>
2024-07-01 04:15:40 +0000

    -

--- src/mech_drive_controller/src/odometry.cpp
+++ src/mech_drive_controller/src/odometry.cpp
@@ -83,7 +83,7 @@ bool Odometry::update(double front_left_pos, double front_right_pos, double back
   linear[0] = (front_left_wheel_est_vel + front_right_wheel_est_vel + back_left_wheel_est_vel + back_right_wheel_est_vel) * left_wheel_radius_ / 4  ;
   linear[1] = (-front_left_wheel_est_vel + front_right_wheel_est_vel - back_left_wheel_est_vel + front_right_wheel_est_vel) * left_wheel_radius_ / 4; 
   // Now there is a bug about scout angular velocity
-  const double angular = (front_right_wheel_est_vel - back_left_wheel_est_vel - front_left_wheel_est_vel + back_right_wheel_est_vel) * left_wheel_radius_ / (8* wheel_separation_);
+  const double angular = (front_right_wheel_est_vel - back_left_wheel_est_vel - front_left_wheel_est_vel + back_right_wheel_est_vel) * left_wheel_radius_ / (4* wheel_separation_);
 
   // Integrate odometry:
   integrateExact(linear, angular);
@@ -102,6 +102,8 @@ bool Odometry::update(double front_left_pos, double front_right_pos, double back
 
   angular_ = angular_accumulator_.getRollingMean();
 
+  std::cout << "linear_x: " << linear_x << ", linear_y: " << linear_y << ", angular: " << angular_ << std::endl;
+  std::cout << "Position: x = " << x_ << ", y = " << y_ << ", heading = " << heading_ << std::endl;
   return true;
 }
 
